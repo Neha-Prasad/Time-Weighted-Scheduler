c2f93ff8f038628967b5cb3115becee83b2bdf59
Neha-Prasad <nshivashankarprasad@gmail.com>
2022-08-22 21:04:30 -0700

    Test data generation

--- src/TestDataGenerator/TestData.java
+++ src/TestDataGenerator/TestData.java
@@ -1,4 +1,21 @@
 package TestDataGenerator;
 
+import java.time.Duration;
+
 public class TestData {
+    private String Name;
+    private Duration ExecutionTime;
+
+    public TestData(String name, Duration executionTime) {
+        Name = name;
+        ExecutionTime = executionTime;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public Duration getExecutionTime() {
+        return ExecutionTime;
+    }
 }
